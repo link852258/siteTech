68fa00a846e66dae6d09e6dea6c7ed72235c1572
jonathan <link852258>
2021-01-15 14:13:52 -0500

    description dans recherche

--- Vues/Recherche/afficherTables.php
+++ Vues/Recherche/afficherTables.php
@@ -1,8 +1,10 @@
 <h1><?php echo $nomDep; ?></h1>
-<?php for($i = 1; $i <= 3; $i++){?>
+<?php $priorites = obtenirPriorite(); ?>
+<?php while($priorite = $priorites->fetch_assoc()){ ?>
 <div class="row">
     <div class="col" id="<?php echo 'mainCol'.$i; ?>">
-        <h2><?php echo 'Priorité '.$i;?></h2>
+        <h2>Priorité <?php echo $priorite['ORDRE']; ?></h2>
+        <h6><?php echo $priorite['NOM']; ?></h6>
         <?php if(isset($_SESSION['offset'])){ ?>
         <?php $res = obtenirTechniciennesSelonDate($_SESSION['IDDepartement'], $i, $_SESSION['dateDebut'],$_SESSION['dateFin'], $_SESSION['offset']); ?>
         <?php $dates = obtenirDateTSSelonDate($_SESSION['IDDepartement'], $_SESSION['dateDebut'],$_SESSION['dateFin'], $_SESSION['offset']); ?>
