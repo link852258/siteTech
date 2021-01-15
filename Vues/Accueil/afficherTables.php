f00339f3610b76934de0bf079305ecbb395ebbef
jonathan <link852258>
2021-01-15 14:11:04 -0500

    ajout de la description

--- Vues/Accueil/afficherTables.php
+++ Vues/Accueil/afficherTables.php
@@ -1,9 +1,11 @@
 <h1><?php echo $nomDep; ?></h1>
-<?php for($i = 1; $i <= 3; $i++){?>
+<?php $priorites = obtenirPriorite(); ?>
+<?php while($priorite = $priorites->fetch_assoc()){ ?>
 <div class="row">
-    <div class="col" id="<?php echo 'mainCol'.$i; ?>">
-        <h2><?php echo 'Priorité '.$i;?></h2>
-        <?php $res = obtenirTS($i, $IDDep); ?>
+    <div class="col" id="<?php echo 'mainCol'.$priorite['ORDRE']; ?>">
+        <h2>Priorité <?php echo $priorite['ORDRE'];?></h2>
+        <h6><?php echo $priorite['NOM']; ?></h6>
+        <?php $res = obtenirTS($priorite['ORDRE'], $IDDep); ?>
         <?php $dates = obtenirDateTS($IDDep); ?>
         <?php require('indexTable.php'); ?>
     </div>
